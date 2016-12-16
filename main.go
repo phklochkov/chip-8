45fb48b5a97abb6caa62e0f2b5a41c48e8bfc957
midasbetankor <www.de31k@gmail.com>
2016-12-16 12:18:15 +0200

    Implementer Sqrt for lulz

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "math"
+)
 
 func main() {
   fmt.Printf("Hello World, from chip-8.\n")
+  fmt.Printf(Sqrt(3.), math.Sqrt(3))
+}
+
+func Power(base float64, power int) float64 {
+  result := 1.
+  for i := 1; i <= power; i++ {
+    result *= base
+  }
+  return result
+}
+
+func Sqrt(x float64) (float64) {
+  start := x / 2.
+
+  precision := 0.0001
+  delta := start
+
+  for delta > precision {
+    var next float64 = start - (Power(start, 2) - x) / 2 * start
+
+    delta = start - next
+
+    if delta < 0 {
+      delta = delta * (-1.)
+    }
+
+    start = next
+  }
+
+  return start
 }
