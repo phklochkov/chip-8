77e8d560d8594cad9a786eac4e8f395d0a0e2870
midasbetankor <www.de31k@gmail.com>
2016-12-20 13:40:30 +0200

    Implemented 'toString'

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "strconv"
+)
+
+// From fmt package, ToString kind of stuff
+type Stringer interface {
+  String() string
+}
+
+type Panda struct {
+  name string
+  age int
+}
+
+// Implementation with correct receiver. Self-note: panda *Panda doesn't work, it should be passed by value
+func (panda Panda) String() string {
+  return panda.name + " it's " + strconv.Itoa(panda.age) + " years old"
+}
 
 func main() {
-  fmt.Printf("Hello World, from chip-8.\n")
+  xiong := Panda{
+    name: "Xiong",
+    age: 3,
+  }
+
+  fmt.Println("Hello World, from", xiong)
 }
