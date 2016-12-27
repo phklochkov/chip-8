ffac4c4d23f209fd78ca2b2cff878aa5071b52b0
midasbetankor <www.de31k@gmail.com>
2016-12-27 23:09:31 +0200

    Refactored tests, implemented CLS

--- src/chip/chip.go
+++ src/chip/chip.go
@@ -3,6 +3,8 @@ package chip
 import (
 	"errors"
 	"fmt"
+	"os"
+	"os/exec"
 	"rom"
 )
 
@@ -16,10 +18,11 @@ type Chip struct {
 func GetMemoryLocation(opcode []byte) uint16 {
 	return (uint16(opcode[0])&15)<<8 | uint16(opcode[1])
 }
+
 func (vm *Chip) Emulate() {
 	//var systemRegister uint16 = 0
 	//fmt.Printf("\nNow running game\n\n%d\n\n", *game)
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 3; i++ {
 		opcode, err := vm.NextOperation()
 		if err != nil {
 			fmt.Println(err)
@@ -34,9 +37,12 @@ func (vm *Chip) Emulate() {
 		switch firstLeft {
 		case 0:
 			switch opcode[1] {
-			case 0:
+			case 0xE0:
+				cmd := exec.Command("cls")
+				cmd.Stdout = os.Stdout
+				cmd.Run()
 				fmt.Println("Clear screen")
-			case 0xE:
+			case 0xEE:
 				fmt.Println("Return")
 			default:
 				fmt.Println("Call RCA or something")
@@ -61,6 +67,8 @@ func (vm *Chip) Emulate() {
 			if vm.VRegisters[lastLeft] == vm.VRegisters[firstRight] {
 				vm.Skip()
 			}
+		case 6:
+			fmt.Println("Set VX = ", lastLeft, opcode[1])
 
 		case 0xA:
 			fmt.Printf("Set register to ")
@@ -83,15 +91,22 @@ func (machine *Chip) Jump(opcode []byte) {
 }
 
 func (vm *Chip) NextOperation() ([]byte, error) {
-	(*vm).OpPointer += 2
-
-	if int(vm.OpPointer) > len((*vm).Rom.Data) {
-		return nil, errors.New("The ROM has ended, STAHP")
+	if len((*vm).Rom.Data) == 0 {
+		return nil, errors.New("You forgot to load ROM :)")
 	}
 
-	opcode := []byte{(*vm).Rom.Data[vm.OpPointer-2], (*vm).Rom.Data[vm.OpPointer-1]}
+	if len((*vm).Rom.Data)-int(vm.OpPointer+1) <= 0 {
+		return nil, errors.New("The ROM has not enough data to fulfill the request")
+	}
+
+	opcode := []byte{(*vm).Rom.Data[vm.OpPointer], (*vm).Rom.Data[vm.OpPointer+1]}
+	(*vm).OpPointer += 2
 
 	return opcode, nil
+}
+
+func (vm *Chip) SetRegister(index, value byte) {
+	(*vm).VRegisters[index] = value
 }
 
 func (vm *Chip) Skip() {
--- src/chip/chip_test.go
+++ src/chip/chip_test.go
@@ -6,6 +6,49 @@ import (
 	"testing"
 )
 
+func TestEmulate(t *testing.T) {
+	testCases := [][]byte{
+		[]byte{0x0A, 0xAA},
+		[]byte{0x12, 0x34},
+	}
+
+	for _, data := range testCases {
+		cartridge := rom.Rom{Data: data}
+		vm := chip.Chip{Rom: cartridge}
+
+		vm.Emulate()
+
+		firstLeft := data[0] >> 4
+		//lastLeft := data[0] & 15
+
+		switch firstLeft {
+		case 0:
+			if vm.OpPointer != 2 {
+				t.Error("0 is unhandled")
+			}
+		case 1:
+			if vm.OpPointer != 0x34 {
+				t.Error("Jump is incorrect")
+			}
+		case 2:
+		case 3:
+			if vm.OpPointer != 4 {
+				t.Error("Eq-Skip is incorrect")
+			}
+		default:
+			t.Error("Unhandled test case")
+
+		}
+	}
+}
+
+func TestSetRegister(t *testing.T) {
+	vm := chip.Chip{}
+	if vm.SetRegister(0xF, 0xFF); vm.VRegisters[0xF] != 0xFF {
+		t.Error("Register is not set", 0xF, 0xFF, vm.VRegisters)
+	}
+}
+
 func TestSkip(t *testing.T) {
 	vm := chip.Chip{}
 	if vm.Skip(); vm.OpPointer != 2 {
@@ -40,20 +83,65 @@ func TestGetMemoryLocation(t *testing.T) {
 
 func TestNextOperation(t *testing.T) {
 	defer fix(t)
-	game := rom.Rom{Data: []byte{42, 42}}
-	vm := chip.Chip{Rom: game}
 
-	code, err := vm.NextOperation()
+	testData := [][]byte{
+		nil,
+		[]byte{},
+		[]byte{42},
+		[]byte{42, 42},
+		[]byte{42, 42, 42},
+	}
 
-	if err != nil {
-		t.Error(err)
+	for _, data := range testData {
+		game := rom.Rom{Data: data}
+		vm := chip.Chip{Rom: game}
+
+		code, err := vm.NextOperation()
+
+		if data == nil && err == nil {
+			t.Error("There's no ROM, Neo")
+		}
+
+		switch len(data) {
+		case 0:
+			if err == nil {
+				t.Error("There's no ROM, Neo")
+			}
+		case 1:
+			if err == nil {
+				t.Error("There's no ROM, Neo")
+			}
+		case 2:
+			if vm.OpPointer != 2 {
+				t.Error("Wrong state modification")
+			}
+			if code[0] != 42 && code[1] != 42 {
+				t.Error("Incorrect data retrieval")
+			}
+		case 3:
+			if vm.OpPointer != 2 {
+				t.Error("Wrong state modification")
+			}
+			if code[0] != 42 && code[1] != 42 {
+				t.Error("Incorrect data retrieval")
+			}
+		default:
+			t.Error("Unhandled test case")
+		}
 	}
-	if code[0] != code[1] && code[0] != 42 {
-		t.Error("Code is retrieved incorrectly")
-	}
-	if vm.OpPointer != 2 {
-		t.Error("Machine state is modified corrupt")
-	}
+	/*
+		game := rom.Rom{Data: testData}
+		vm := chip.Chip{Rom: game}
+
+		code, err := vm.NextOperation()
+
+		if err != nil {
+			t.Error(err)
+		}
+		if code[0] != 42 {
+			t.Error("NextOperation is incorrect")
+		}
+	*/
 }
 
 func fix(t *testing.T) {
